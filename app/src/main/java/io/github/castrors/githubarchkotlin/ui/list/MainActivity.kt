53db5bfeafc0d37496bfb96bdb343943c5c9ee3d
Rodrigo Castro <rodrigodesouzacastro@gmail.com>
2018-01-17 21:36:42 -0200

    Add swipe to refresh on main activity

--- app/src/main/java/io/github/castrors/githubarchkotlin/ui/list/MainActivity.kt
+++ app/src/main/java/io/github/castrors/githubarchkotlin/ui/list/MainActivity.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
@@ -13,7 +14,9 @@ import io.github.castrors.githubarchkotlin.ui.detail.DetailActivity
 import io.github.castrors.githubarchkotlin.utilities.InjectorUtils
 import io.github.castrors.githubarchkotlin.utilities.putBundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+    private lateinit var factory: MainViewModelFactory
+    private lateinit var viewModel : MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,13 +24,18 @@ class MainActivity : AppCompatActivity() {
 
         setupToolbar()
 
-        val factory = InjectorUtils.provideMainActivityViewModelFactory(this.applicationContext)
-        val viewModel = ViewModelProviders.of(this, factory).get(MainActivityViewModel::class.java)
+        factory = InjectorUtils.provideMainActivityViewModelFactory(this.applicationContext)
+        viewModel = ViewModelProviders.of(this, factory).get(MainActivityViewModel::class.java)
 
         val listAdapter = RepoListAdapter(ArrayList(0), this, { repo ->
             startActivity(Intent(this, DetailActivity::class.java)
                     .putBundle(repo.owner.login, repo.name))
         })
+        val swipeToRefresh = findViewById<SwipeRefreshLayout>(R.id.refreshLayout)
+        swipeToRefresh.setOnRefreshListener{
+            viewModel.forceUpdate()
+        }
+
         val recyclerView = findViewById<RecyclerView>(R.id.repoList)
         recyclerView.adapter = listAdapter
 
@@ -35,6 +43,9 @@ class MainActivity : AppCompatActivity() {
             repoEntries?.let {
                 listAdapter.repoList = it
             }
+        })
+        viewModel.isLoadingLiveData.observe(this, Observer<Boolean> {
+            it?.let { swipeToRefresh.isRefreshing = it }
         })
     }
 
--- app/src/main/java/io/github/castrors/githubarchkotlin/ui/list/MainActivityViewModel.kt
+++ app/src/main/java/io/github/castrors/githubarchkotlin/ui/list/MainActivityViewModel.kt
@@ -1,10 +1,19 @@
 package io.github.castrors.githubarchkotlin.ui.list
 
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MediatorLiveData
 import android.arch.lifecycle.ViewModel
 import io.github.castrors.githubarchkotlin.data.GithubRepository
 import io.github.castrors.githubarchkotlin.data.database.Repo
 
 internal class MainActivityViewModel(private val repository: GithubRepository) : ViewModel() {
     val repoList: LiveData<List<Repo>> = repository.githubRepositoriesList
+
+    var isLoadingLiveData = MediatorLiveData<Boolean>().apply {
+        this.addSource(repoList) { this.value = false }
+    }
+
+    fun forceUpdate() {
+        repository.githubRepositoriesList
+    }
 }
